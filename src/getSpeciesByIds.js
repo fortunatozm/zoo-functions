bd548beb368c70eea952ce0fd9b182e3a6c50d35
fortunatozm <fortunatompongo@hotmail.com>
2022-01-25 19:37:19 -0300

    Requisito - 1 e 2 - OK

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -10,6 +10,4 @@ function getSpeciesByIds(...ids) {
   return species.filter((animalId) => ids.includes(animalId.id));
 }
 
-// console.log(species);
-console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce', 'e8481c1d-42ea-4610-8e11-1752cfc05a46'));
 module.exports = getSpeciesByIds;
