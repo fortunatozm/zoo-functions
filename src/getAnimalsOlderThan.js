9b0aa0bcd4e2c02de753d1d840377f3ad904b8a7
fortunatozm <fortunatompongo@hotmail.com>
2022-01-25 19:25:26 -0300

    Requisito - 2 - OK

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -3,12 +3,9 @@ const { species } = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
-  // console.log();
   const pNames = species.find((pName) => pName.name === animal);
   return pNames.residents.every((pAge) => pAge.age >= age);
-
-  // return pNames;
 }
 
-console.log(getAnimalsOlderThan('lions', 7));
+// console.log(getAnimalsOlderThan('lions', 7));
 module.exports = getAnimalsOlderThan;
