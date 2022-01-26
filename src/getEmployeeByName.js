04f4b32d93bed684fe07886751a1c7524c0d1d4b
fortunatozm <fortunatompongo@hotmail.com>
2022-01-26 08:49:07 -0300

    Requisito - 3 - OK

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,18 @@
 const data = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  const checks = employees.some((check) => check.lastName === employeeName || check.firstName === employeeName);
+  if (employeeName !== undefined && employeeName !== null && checks === true) {
+    const aP = employees.filter((employee) => employee.firstName === employeeName
+    || employee.lastName === employeeName);
+    return aP.reduce((namee) => namee);
+  }
+  const Rname = [];
+  return { ...Rname };
 }
 
+console.log(getEmployeeByName('Burl'));
+// console.log(employees);
 module.exports = getEmployeeByName;
