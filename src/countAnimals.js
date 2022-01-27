6ea43a68a1a7484c25feb2a9e6149c9451d6aa4f
fortunatozm <fortunatompongo@hotmail.com>
2022-01-27 20:44:45 -0300

    Requisito - 5 - OK

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,20 @@
 const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 function countAnimals(animal) {
   // seu código aqui
+  if (animal === undefined) {
+    const result = {};
+    species.forEach((specie) => { result[specie.name] = specie.residents.length; });
+    return result;
+  }
+  if (animal.sex === null || animal.sex === undefined) {
+    const specieFilter = species.find((specie) => specie.name === animal.specie).residents.length;
+    return specieFilter;
+  }
+  const spFilSex = species.find((specie2) => specie2.name
+  === animal.specie).residents.filter((sexLen) => sexLen.sex === 'female');
+  return spFilSex.length;
 }
-
+console.log(countAnimals({ specie: 'giraffes', sex: 'female' }));
 module.exports = countAnimals;
