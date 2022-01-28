2b3b6c480067831f074f535bf24306e2f2b96919
fortunatozm <fortunatompongo@hotmail.com>
2022-01-27 23:37:47 -0300

    Requisito - 6 - OK

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -2,10 +2,26 @@ const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
   // seu código aqui
+  if (entrants !== null && entrants !== undefined && entrants.length !== 0) {
+    const filMaior18 = entrants.filter((fM18) => fM18.age >= 18 && fM18.age < 50);
+    const resulM18 = filMaior18.length;
+    const filMenor18 = entrants.filter((fMe18) => fMe18.age < 18);
+    const resulMe18 = filMenor18.length;
+    const filIdos = entrants.filter((filIdo) => filIdo.age >= 50);
+    const resulIdos = filIdos.length;
+    return { child: resulMe18, adult: resulM18, senior: resulIdos };
+  }
+  return 0;
 }
 
 function calculateEntry(entrants) {
   // seu código aqui
+  if (entrants !== null && entrants !== undefined && Object.keys(entrants).length !== 0) {
+    const chamaFunc = countEntrants(entrants);
+    const total = chamaFunc.child * 20.99 + chamaFunc.senior * 24.99 + chamaFunc.adult * 49.99;
+    return total;
+  }
+  return 0;
 }
 
 module.exports = { calculateEntry, countEntrants };
